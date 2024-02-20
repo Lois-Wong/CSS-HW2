d259a0832733803032fe524b5b60af91f87d7538
mollorc1 <robert.molloy@jhuapl.edu>
2024-02-19 23:27:51 -0500

    please be the last time I change this

--- build_new_csv.py
+++ build_new_csv.py
@@ -4,12 +4,11 @@ original = pd.read_csv('data/common2.csv')
 new = pd.DataFrame(columns = ['comment'])
 random.seed(12)
 repeats = []
-for i in range(40):
+
+while len(repeats) < 40:
     exact = original.sample(2, replace=False)
     text = f"Response 1: {exact['comment'].values[0]} \n Response 2: {exact['comment'].values[1]}"
-    if text in repeats:
-        i = i-1
-    else:
+    if text not in repeats:
         new.loc[len(new.index)] = [text]
         repeats.append(text)
 new.to_csv('new_common2.csv', index=True)
